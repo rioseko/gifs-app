2af0f7fd926af9c729da0dd2f582be4400e358ef
Juan Rioseco Z <jrioseco@emeal.nttdata.com>
2023-09-12 17:51:53 -0300

    refactor http get parameters

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Gif } from './../interfaces/gifs.interfaces';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SearchGifResponse } from '../interfaces/gifs.interfaces';
@@ -10,13 +10,12 @@ import { SearchGifResponse } from '../interfaces/gifs.interfaces';
 export class GifsService {
   private _history: string[] = [];
   private _apiKey: string = 'K1TP3l79EczJc8bIdzij54BJ5MZ5o5Jd';
+  private _baseUrl: string = 'https://api.giphy.com/v1/gifs';
   public results: Gif[] = [];
 
   constructor(private http: HttpClient) {
-      
-      this._history = JSON.parse(localStorage.getItem('history')!) || [];
-      this.results = JSON.parse(localStorage.getItem('lastResult')!) || [];
-
+    this._history = JSON.parse(localStorage.getItem('history')!) || [];
+    this.results = JSON.parse(localStorage.getItem('lastResult')!) || [];
   }
 
   get history() {
@@ -26,16 +25,20 @@ export class GifsService {
   searchGifs = (item: string = ''): void => {
     item = item.trim().toLowerCase();
     if (!this._history.includes(item)) {
-
       this._history.unshift(item);
       this._history = this._history.splice(0, 10);
 
       localStorage.setItem('history', JSON.stringify(this._history));
     }
 
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit', '10')
+      .set('q', item);
+
     this.http
       .get<SearchGifResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=K1TP3l79EczJc8bIdzij54BJ5MZ5o5Jd&q=${item}&limit=10`
+        `${this._baseUrl}/search`, { params }
       )
       .subscribe((response) => {
         console.log(response.data);
